44f7afb08a0c1e095c0017bee1b44eee386c9157
Juan M Ortiz <juanmoo@me.com>
2020-11-27 13:57:21 -0500

    Create Agents in world and define printing functions

--- amongus/agent.py
+++ amongus/agent.py
@@ -1,14 +1,19 @@
 from amongus.model import Model
+import numpy as np
 
 class Agent:
   model = Model()
   
-  def __init__(self, id, world, vision_radius=3):
+  def __init__(self, id, world, start_location, vision_radius=3):
     self.id = id
     self.world = world
     self.alive = True
-    self.location = (0,0) #change later
+    self.location = start_location
     self.vision_radius = vision_radius
+    self.agent_type = 'Undefined'
+
+  def distance_to(self, loc):
+    return np.linalg.norm(self.location - loc)
 
   def get_action():
     pass
@@ -32,13 +37,20 @@ class Agent:
     if move_action >= 5 and move_action <= 7:
         self.location[0] = max(0, self.location[0]-1)
     return self.location
+
+
+  def __repr__(self) -> str:
+    return 'ID: {}, Role: {}, Location: ({}, {})'.format(self.id, self.agent_type, *self.location)
+
     
 
 class Crewmate(Agent):
-  def __init__(self, id, world, vision_radius=3):
-    super().__init__(id, world, vision_radius)
+  def __init__(self, id, world, start_location, vision_radius=3):
+    super().__init__(id, world, start_location, vision_radius)
+    self.agent_type = 'Crewmate'
 
 class Impostor(Agent):
-  def __init__(self, id, world, vision_radius=3, kill_distance=1):
-    super().__init__(id, world, vision_radius)
+  def __init__(self, id, world, start_location, vision_radius=3, kill_distance=1):
+    super().__init__(id, world, start_location, vision_radius)
+    self.agent_type = 'Impostor'
     self.kill_distance = kill_distance
--- amongus/game.py
+++ amongus/game.py
@@ -1,26 +1,49 @@
+from amongus.agent import Crewmate, Impostor
+from amongus.world import World
+import random
+import numpy as np
+
 class Game:
-  def __init__(self, map_size, num_tasks, num_agents, max_steps):
+
+  # Default Game Parameters
+  params = {
+    'kill_distance': 1,
+    'agent_vision_radius': 3,
+    'task_reward': 3
+  }
+
+  def __init__(self, map_size, num_tasks, num_agents, max_steps, params=params):
     self.n = map_size
     self.t = num_tasks
     self.num_agents = num_agents
     self.max_steps = max_steps
-    #Arbitrary numbers
-    self.kill_distance = 1
-    self.agent_vision_radius = 3
-    self.task_reward = 3
+    
+    # Game Parameters
+    self.kill_distance = params.get('kill_distance', Game.params['kill_distance'])
+    self.agent_vision_radius = params.get('agent_vision_radius', Game.params['agent_vision_radius'])
+    self.task_reward = params.get('task_reward', Game.params['task_reward'])
+
     self.reset()
 
   def reset(self):
     # Generate a new world
     self.world = World(self.n, self.t)
-    # Make all but one crewmates
+
+    # Pick num_agents different map locations
+    rng = np.random.default_rng()
+    locs = rng.choice(self.n * self.n, size=self.num_agents, replace=False)
+    coord_map = np.vectorize(lambda x: (x//self.n, x%self.n))
+    locs = np.vstack(coord_map(locs)).T
+
+    # Instatntiate num_agents - 1 Crewmates and 1 Impostor agents
     self.impostor_index = random.randint(0, self.num_agents-1)
     self.agents = []
     for i in range(self.num_agents):
         if i == self.impostor_index:
-            self.agents.append(Impostor(i, self.world, self.agent_vision_radius, self.kill_distance))
+            self.agents.append(Impostor(i, self.world, locs[i], vision_radius=self.agent_vision_radius, kill_distance=self.kill_distance))
         else:
-            self.agents.append(Crewmate(i, self.world, self.agent_vision_radius))
+            self.agents.append(Crewmate(i, self.world, locs[i], vision_radius=self.agent_vision_radius))
+
     # Whether the game is ongoing
     self.ongoing = True
     self.num_steps = 0
@@ -36,25 +59,31 @@ class Game:
         # Dead agents cannot do anything
         if not agent.alive:
             continue 
+
         action = agent.get_action()
         # Assume action to be a row vector 
         # Index 0 represents kill(0) or move(1)
         # Index 1 represents which direction to move
         # Index 2 represents who to kill
         # Remainder of the k*(n-1) indices represent messages to each other agent
+
         if action[0] == 0:
             # Only the impostor can kill someone else 
             if agent.id != self.impostor_index:
                 continue
+
             # We are killing this person, they die in the next round
-            to_kill = action[2]
-            can_kill = check_proximity(to_kill) #implement checking that the person is close enough to kill
+            to_kill = action[2] # We migh want to just kill whoever is within distance for the impostor ?
+            can_kill = agent.distance_to(self.agents[to_kill]) <= self.kill_distance
+
             if can_kill:
                 pass
                 # rewards[agent]
+
             else:
                 to_kill = None
-        if action[0] == 1:
+
+        elif action[0] == 1:
             # We are moving
             new_location = agent.update_location(action[1])
             task_done = self.world.check_task_accomplished(new_location)
@@ -65,7 +94,6 @@ class Game:
     if self.num_steps >= self.max_steps:
         self.ongoing = False
         
-    pass
 
   def get_observation(self, agent):
     # Get a 2*vision_radius+1 size square representing what the agent can see
@@ -76,7 +104,6 @@ class Game:
     # award the agent for doing the task
     # If the agent is an impostor and they just killed someone, award the agent 
     # for killing them
-    
     
     pass
   
@@ -92,3 +119,28 @@ class Game:
     # Actions are move or kill 
     
     pass
+
+  def __repr__(self) -> str:
+    world_arr = np.array(self.world.map, dtype=object)
+    for agent in self.agents:
+      char = 'C' if agent.agent_type == 'Crewmate' else 'I'
+      r, c = agent.location
+      world_arr[r, c] = char
+    
+    def make_blank(x):
+      if x == 0.0:
+        return ''
+      elif x == 1.0:
+        return 'G'
+      else:
+        return x
+
+    make_blank = np.vectorize(make_blank)
+    world_arr = make_blank(world_arr)
+
+    out = '-' * (4 * len(world_arr[0]) + 2) + '\n'
+    for row in world_arr:
+      fstring = '{:^3s}|' * len(row)
+      out += '|' + fstring[:-1].format(*row) + '|' + '\n'
+      out += '-' * (4 * len(row) + 2) + '\n'
+    return out
